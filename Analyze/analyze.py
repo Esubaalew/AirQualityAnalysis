deee4d745c54922f42933fcbf3b9a52602dafaeb
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-05-30 20:37:22 +0300

    added  get monthly trends function

--- Analyze/analyze.py
+++ Analyze/analyze.py
@@ -1,5 +1,7 @@
 import sys
+import pandas as pd
 sys.path.append('../')
+
 
 from tools import get_clean_data
 
@@ -17,3 +19,21 @@ def get_most_common_pollutants(data, top_n=10):
     pollutant_counts = data.groupby('Name').size().reset_index(name='Count')
     most_common_pollutants = pollutant_counts.sort_values(by='Count', ascending=False)
     return most_common_pollutants.head(top_n)
+
+
+def get_monthly_trends(data):
+    """
+    Calculate monthly average pollutant levels.
+
+    Parameters:
+    data (DataFrame): The input DataFrame containing air pollutant data.
+
+    Returns:
+    DataFrame: A DataFrame containing monthly average pollutant levels.
+    """
+    data['Month'] = data['Start_Date'].dt.to_period('M')
+    monthly_trends = data.groupby('Month')['Data Value'].mean().reset_index()
+    monthly_trends['Month'] = monthly_trends['Month'].dt.to_timestamp()
+    return monthly_trends
+
+
